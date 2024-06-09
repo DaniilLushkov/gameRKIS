2f4dfdb173a411ef0430cdbb9198d47d02ad2051
DaniilLushkov <150797253+DaniilLushkov@users.noreply.github.com>
2024-06-09 21:06:10 +0300

    Add files via upload

---
+++ GameCanvas/html/Projects/Tiles/tile1.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile1" tilewidth="1024" tileheight="1024" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="288" height="168" source="C:/Users/Даниил/Downloads/[v1.0] tranquil_tunnels.png"/>
+ </tile>
+ <tile id="1">
+  <image width="296" height="232" source="C:/Users/Даниил/Downloads/[v1.1] tranquil_tunnels_legacy.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="288" source="C:/Users/Даниил/Downloads/tranquil_tunnels_preview_map.png"/>
+ </tile>
+ <tile id="3">
+  <image width="296" height="232" source="C:/Users/Даниил/Downloads/[v1.1] tranquil_tunnels_darker.png"/>
+ </tile>
+ <tile id="4">
+  <image width="296" height="232" source="C:/Users/Даниил/Downloads/[v1.1] tranquil_tunnels_transparent.png"/>
+ </tile>
+ <tile id="5">
+  <image width="480" height="328" source="C:/Users/Даниил/Downloads/[v1.2] tranquil_tunnels_darker.png"/>
+ </tile>
+ <tile id="6">
+  <image width="480" height="328" source="C:/Users/Даниил/Downloads/[v1.2] tranquil_tunnels_legacy.png"/>
+ </tile>
+ <tile id="7">
+  <image width="480" height="328" source="C:/Users/Даниил/Downloads/[v1.2] tranquil_tunnels_transparent.png"/>
+ </tile>
+ <tile id="8">
+  <image width="200" height="96" source="C:/Users/Даниил/Downloads/font_impossible.png"/>
+ </tile>
+ <tile id="9">
+  <image width="112" height="32" source="C:/Users/Даниил/Downloads/smolfont.png"/>
+ </tile>
+ <tile id="10">
+  <image width="1024" height="1024" source="C:/Users/Даниил/Downloads/[v1.3] tranquil_tunnels_darker.png"/>
+ </tile>
+ <tile id="11">
+  <image width="1024" height="1024" source="C:/Users/Даниил/Downloads/[v1.3] tranquil_tunnels_transparent.png"/>
+ </tile>
+</tileset>
---
+++ GameCanvas/html/Projects/Tiles/tile4.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile4" tilewidth="1800" tileheight="1200" tilecount="11" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Day/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Day/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Day/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Day/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Day/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Night/1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Night/2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Night/3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Night/4.png"/>
+ </tile>
+ <tile id="9">
+  <image width="576" height="324" source="../../Tilesets/2 Background/Night/5.png"/>
+ </tile>
+ <tile id="10">
+  <image width="1800" height="1200" source="../../Tilesets/2 Background/Overlay.png"/>
+ </tile>
+</tileset>
